12abff14cf42defad3e172ccdf5b648e3fbef97b
justinro-underscore <jcroderman@gmail.com>
2021-04-29 18:25:24 -0500

    Puts background on body

--- src/App.js
+++ src/App.js
@@ -8,18 +8,14 @@ import Navigation from './navigation/Navigation';
 import Home from './pages/home/Home';
 import Projects from './pages/projects/Projects';
 
-import {Container} from './App.styled';
-
 export default function App() {
   return (
-    <Container>
-      <BrowserRouter>
-        <Navigation />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/projects' component={Projects} />
-        </Switch>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <Navigation />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/projects' component={Projects} />
+      </Switch>
+    </BrowserRouter>
   );
 }
--- src/App.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import {gray0} from './styles/palette';
-
-export const Container = styled.div`
-  background-color: ${gray0};
-`;
