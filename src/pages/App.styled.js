1f4bf510d13093356a76c47824196b96c13c4541
justinro-underscore <jcroderman@gmail.com>
2021-04-29 08:58:17 -0500

    Refactors codebase to define basic structure

--- src/pages/App.styled.js
+++ src/pages/App.styled.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
+import {gray0, bluelight, black} from '../styles/palette'
 
 export const AppContainer = styled.div`
   text-align: center;
 `;
 
 export const AppHeader = styled.header`
-  background-color: #282c34;
+  background-color: ${gray0};
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
+  color: ${black};
 `;
 
 export const AppLink = styled.a`
-  color: #61dafb;
+  color: ${bluelight};
 `;
---
+++ src/styles/palette.js
@@ -0,0 +1,16 @@
+/**
+ * Define the theme for the website
+ */
+
+export const bluedark = '#3D70B2';
+export const bluelight = '#5596E6';
+export const green = '#41D6C3';
+
+export const white = '#FFFFFF';
+
+export const gray0 = '#F5F7FA';
+export const gray1 = '#F0F3F6';
+export const gray2 = '#DFE3E6';
+export const gray3 = '#8C9BA5';
+
+export const black = '#000000';
